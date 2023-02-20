53444aeec6de3c95f1e91cbcbf65ce0c116b25e3
calebatsu <calebatsu@hotmail.it>
2023-02-20 10:06:11 +0000

    modified the footer to match my mockup

--- components/sr-footer.vue
+++ components/sr-footer.vue
@@ -58,46 +58,20 @@
           </div>
         </div>
         <div class="mx-6 py-10 text-center md:text-left">
-          <div class="grid grid-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-            <div class="">
-              <h6 class="
-                  uppercase
-                  font-semibold
-                  mb-4
-                  flex
-                  items-center
-                  justify-center
-                  md:justify-start
-                ">
-                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="cubes"
-                  class="w-4 mr-3" role="img" xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 512 512">
-                  <path fill="currentColor"
-                    d="M488.6 250.2L392 214V105.5c0-15-9.3-28.4-23.4-33.7l-100-37.5c-8.1-3.1-17.1-3.1-25.3 0l-100 37.5c-14.1 5.3-23.4 18.7-23.4 33.7V214l-96.6 36.2C9.3 255.5 0 268.9 0 283.9V394c0 13.6 7.7 26.1 19.9 32.2l100 50c10.1 5.1 22.1 5.1 32.2 0l103.9-52 103.9 52c10.1 5.1 22.1 5.1 32.2 0l100-50c12.2-6.1 19.9-18.6 19.9-32.2V283.9c0-15-9.3-28.4-23.4-33.7zM358 214.8l-85 31.9v-68.2l85-37v73.3zM154 104.1l102-38.2 102 38.2v.6l-102 41.4-102-41.4v-.6zm84 291.1l-85 42.5v-79.1l85-38.8v75.4zm0-112l-102 41.4-102-41.4v-.6l102-38.2 102 38.2v.6zm240 112l-85 42.5v-79.1l85-38.8v75.4zm0-112l-102 41.4-102-41.4v-.6l102-38.2 102 38.2v.6z">
-                  </path>
-                </svg>
-                Tailwind ELEMENTS
-              </h6>
-              <p>
-                Here you can use rows and columns to organize your footer content. Lorem ipsum dolor
-                sit amet, consectetur adipisicing elit.
-              </p>
-            </div>
+          <div class="grid grid-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+
             <div class="">
               <h6 class="uppercase font-semibold mb-4 flex justify-center md:justify-start">
-                Products
+                Get Started
               </h6>
               <p class="mb-4">
-                <a href="#!" class="text-gray-600">Angular</a>
+                <a href="#!" class="text-gray-600">Home</a>
               </p>
               <p class="mb-4">
-                <a href="#!" class="text-gray-600">React</a>
+                <a href="#!" class="text-gray-600">Browse</a>
               </p>
               <p class="mb-4">
-                <a href="#!" class="text-gray-600">Vue</a>
-              </p>
-              <p>
-                <a href="#!" class="text-gray-600">Laravel</a>
+                <a href="#!" class="text-gray-600">About</a>
               </p>
             </div>
             <div class="">
@@ -105,16 +79,13 @@
                 Useful links
               </h6>
               <p class="mb-4">
-                <a href="#!" class="text-gray-600">Pricing</a>
+                <a href="#!" class="text-gray-600">Terms of use</a>
               </p>
               <p class="mb-4">
-                <a href="#!" class="text-gray-600">Settings</a>
+                <a href="#!" class="text-gray-600">Privacy Policy</a>
               </p>
               <p class="mb-4">
-                <a href="#!" class="text-gray-600">Orders</a>
-              </p>
-              <p>
-                <a href="#!" class="text-gray-600">Help</a>
+                <a href="#!" class="text-gray-600">Contact</a>
               </p>
             </div>
             <div class="">
@@ -128,7 +99,7 @@
                     d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z">
                   </path>
                 </svg>
-                New York, NY 10012, US
+                London, LDN LN1, UK
               </p>
               <p class="flex items-center justify-center md:justify-start mb-4">
                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="envelope"
@@ -138,7 +109,7 @@
                     d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z">
                   </path>
                 </svg>
-                info@example.com
+                speedyrentals@gmail.com
               </p>
               <p class="flex items-center justify-center md:justify-start mb-4">
                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="phone"
@@ -150,22 +121,12 @@
                 </svg>
                 + 01 234 567 88
               </p>
-              <p class="flex items-center justify-center md:justify-start">
-                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="print"
-                  class="w-4 mr-4" role="img" xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 512 512">
-                  <path fill="currentColor"
-                    d="M448 192V77.25c0-8.49-3.37-16.62-9.37-22.63L393.37 9.37c-6-6-14.14-9.37-22.63-9.37H96C78.33 0 64 14.33 64 32v160c-35.35 0-64 28.65-64 64v112c0 8.84 7.16 16 16 16h48v96c0 17.67 14.33 32 32 32h320c17.67 0 32-14.33 32-32v-96h48c8.84 0 16-7.16 16-16V256c0-35.35-28.65-64-64-64zm-64 256H128v-96h256v96zm0-224H128V64h192v48c0 8.84 7.16 16 16 16h48v96zm48 72c-13.25 0-24-10.75-24-24 0-13.26 10.75-24 24-24s24 10.74 24 24c0 13.25-10.75 24-24 24z">
-                  </path>
-                </svg>
-                + 01 234 567 89
-              </p>
             </div>
           </div>
         </div>
         <div class="text-center p-6 bg-gray-200">
-          <span>© 2021 Copyright:</span>
-          <a class="text-gray-600 font-semibold" href="https://tailwind-elements.com/">Tailwind Elements</a>
+          <span>© 2023 Copyright:</span>
+          <a class="text-gray-600 font-semibold" href="https://tailwind-elements.com/">SpeedyRentals</a>
         </div>
       </footer>
 </template>
