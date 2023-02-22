d3b564163122d4e25c0583a478679232b009e8ea
calebatsu <calebatsu@hotmail.it>
2023-02-22 21:31:14 +0000

    added input elements on top of jumbotrone

--- components/sr-jumbotrone.vue
+++ components/sr-jumbotrone.vue
@@ -4,15 +4,19 @@
         <div class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed"
             style="background-color: rgba(0, 0, 0, 0.6)">
             <div class="flex h-full items-center justify-center">
-                <div class="text-white">
-                    <h2 class="mb-4 text-4xl font-semibold">Fast and easy way to share and rent a car</h2>
+                <div class="text-white space-x-2 ">
+                    <h2 class="mb-4 text-4xl font-semibold">Fast and easy way to <u>share</u> and <u>rent</u>  a car</h2>
                     <h4 class="mb-6 text-xl font-semibold">Subheading</h4>
+                    <input type="text" placeholder="Location" class="rounded border-2 border-neutral-50 px-3 pt-[10px] pb-[8px] text-sm font-medium uppercase leading-normal text-neutral-50 bg-transparent hover:text-neutral-500 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10">
+                    <input type="date" class="rounded border-2 border-neutral-50 px-1 pt-[10px] pb-[8px] text-sm font-medium uppercase leading-normal text-neutral-50 bg-transparent hover:text-neutral-500 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10">
+                    <input type="date" class="rounded border-2 border-neutral-50 px-1 pt-[10px] pb-[8px] text-sm font-medium uppercase leading-normal text-neutral-50 bg-transparent hover:text-neutral-500 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10">
+                    
                     <button type="button"
-                        class="rounded border-2 border-neutral-50 px-7 pt-[10px] pb-[8px] text-sm font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+                        class="rounded border-2 border-neutral-50 px-1 pt-[10px] pb-[8px] text-sm font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
                         data-te-ripple-init data-te-ripple-color="light">
                         Find your drive
                     </button>
-                    
+
                 </div>
                 
             </div>
