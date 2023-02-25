d3ba384868ffdfb74342ddf4793f1ba76062edfe
calebatsu <calebatsu@hotmail.it>
2023-02-25 09:47:09 +0000

    created, contact,terms and policy page

---
+++ pages/privacy.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        i'm privacy policy
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/terms.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        im terms
+
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
