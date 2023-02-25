1983fc44f5def4ac941959bfe1caf86c472acb2c
calebatsu <calebatsu@hotmail.it>
2023-02-25 09:48:38 +0000

    added context and layout to the about us componenent

---
+++ components/sr-about.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+        <!-- Container for demo purpose -->
+<div class="container my-24 px-6 mx-auto">
+
+<!-- Section: Design Block -->
+<section class="mb-32 text-gray-800">
+  <h2 class="text-2xl font-bold mb-12 text-center">About SpeedyRentals</h2>
+
+  <div class="flex flex-wrap mb-12">
+    <div class="grow-0 shrink-0 basis-auto w-full lg:w-6/12 lg:pr-6 mb-6 lg:mb-0">
+      <div class="relative overflow-hidden bg-no-repeat bg-cover ripple shadow-lg rounded-lg"
+        style="background-position: 50%" data-mdb-ripple="true" data-mdb-ripple-color="light">
+        <img src="https://mdbootstrap.com/img/new/standard/city/028.jpg" class="w-full" alt="Louvre" />
+        <a href="#!">
+          <div
+            class="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out"
+            style="background-color: rgba(251, 251, 251, 0.2)"></div>
+        </a>
+      </div>
+    </div>
+
+    <div class="grow-0 shrink-0 basis-auto w-full lg:w-6/12 lg:pl-6">
+      <h3 class="text-2xl font-bold mb-4">Our Mission</h3>
+      <p class="text-gray-500 mb-6">
+        At SpeedyRentals, our mission is to make car rental a seamless and hassle-free experience
+         for our customers. We believe in empowering people to share their vehicles with others, 
+         thereby creating a sustainable and cost-effective way of traveling. Our platform allows car
+          owners to list their vehicles for rent, while renters can choose from a wide range of vehicles 
+          at affordable rates. We strive to create a community of trust and reliability, where car owners 
+          and renters can interact with ease and confidence.
+      </p>
+    </div>
+  </div>
+
+  <div class="flex flex-wrap lg:flex-row-reverse mb-12">
+    <div class="grow-0 shrink-0 basis-auto w-full lg:w-6/12 lg:pl-6 mb-6 lg:mb-0">
+      <div class="relative overflow-hidden bg-no-repeat bg-cover ripple shadow-lg rounded-lg"
+        style="background-position: 50%" data-mdb-ripple="true" data-mdb-ripple-color="light">
+        <img src="https://mdbootstrap.com/img/new/standard/city/033.jpg" class="w-full" alt="Louvre" />
+        <a href="#!">
+          <div
+            class="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out"
+            style="background-color: rgba(251, 251, 251, 0.2)"></div>
+        </a>
+      </div>
+    </div>
+    <div class="grow-0 shrink-0 basis-auto w-full lg:w-6/12 lg:pr-6">
+      <h3 class="text-2xl font-bold mb-4">Our Team</h3>
+      <p class="text-gray-500">
+        Our team is made up of passionate and experienced professionals who are dedicated to making SpeedyRentals the best peer-to-peer car rental platform in the market.
+        We have a diverse team of experts in software engineering, marketing, customer service, and operations. 
+        Our team members bring unique skills and perspectives, which help us to innovate and provide exceptional service to our customers. 
+        We are committed to ensuring that our platform is easy to use, secure, and reliable, and we are always looking for ways to improve the user experience.
+      </p>
+    </div>
+  </div>
+</section>
+<!-- Section: Design Block -->
+
+</div>
+<!-- Container for demo purpose -->
+
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
