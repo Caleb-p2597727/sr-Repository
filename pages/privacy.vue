4d1ccd3feb131cff4dc075b7ba55fef0f67efffb
calebatsu <calebatsu@hotmail.it>
2023-02-25 10:55:25 +0000

    composed privacy page

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -1,13 +1,52 @@
 <template>
     <div>
-        i'm privacy policy
+    <body class="bg-gray-100">
+        <div class="max-w-4xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
+            <h1 class="text-2xl font-bold mb-4">Privacy Policy</h1>
+            <p class="mb-4">At SpeedyRentals, we take your privacy seriously. This Privacy Policy explains how we collect,
+                use, and share your personal information when you use our website and services.</p>
+
+            <h2 class="text-lg font-bold mb-2">Information We Collect</h2>
+            <p class="mb-4">When you use SpeedyRentals, we collect information about you that you provide to us, such as
+                your name, email address, phone number, and payment information. We also collect information about your use
+                of our website and services, such as your search queries, rental history, and device information.</p>
+
+            <h2 class="text-lg font-bold mb-2">How We Use Your Information</h2>
+            <p class="mb-4">We use your personal information to provide and improve our services, to communicate with you,
+                to process payments, and to comply with legal obligations. We may also use your information to personalize
+                your experience, to analyze usage patterns, and to display relevant advertising.</p>
+
+            <h2 class="text-lg font-bold mb-2">How We Share Your Information</h2>
+            <p class="mb-4">We may share your personal information with third-party service providers who help us to provide
+                our services, such as payment processors and insurance providers. We may also share your information with
+                law enforcement or other government agencies if required by law. We do not sell your personal information to
+                third parties.</p>
+
+            <h2 class="text-lg font-bold mb-2">Security</h2>
+            <p class="mb-4">We take reasonable measures to protect your personal information from unauthorized access, use,
+                or disclosure. However, no method of transmission over the internet or electronic storage is completely
+                secure, so we cannot guarantee absolute security.</p>
+
+            <h2 class="text-lg font-bold mb-2">Your Rights</h2>
+            <p class="mb-4">You have the right to access, correct, or delete your personal information. You also have the
+                right to object to or restrict our processing of your personal information, and to receive your information
+                in a portable format. To exercise these rights, please contact us using the information below.</p>
+
+            <h2 class="text-lg font-bold mb-2">Contact Us</h2>
+            <p>If you have any questions or concerns about our Privacy Policy, please contact us at:</p>
+            <ul class="list-disc ml-4 mb-4">
+                <li>SpeedyRentals, Inc.</li>
+                <li>1234 Main St.</li>
+                <li>Anytown, USA 12345</li>
+                <li>Email: privacy@speedyrentals.com</li>
+            </ul>
     </div>
+</body>
+</div>
 </template>
 
 <script setup>
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
