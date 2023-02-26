4ef2089f261cc36393f09fa77e4ed6c4183d098d
calebatsu <calebatsu@hotmail.it>
2023-02-26 21:53:04 +0000

    new page dynamic

---
+++ pages/car/[name]/[id].vue
@@ -0,0 +1,44 @@
+
+<template>
+    <div>
+        <!-- CAR DETAIL PAGE -->
+        <div>
+            <Sr-carDetail :image="car.url" :model=car.name :seats=car.seats :rate="car.price" :miles="car.miles" ></Sr-carDetail>
+        </div>
+        <SrCarAttributes></SrCarAttributes>
+        <Sr-carDescription description="hello"></Sr-carDescription>
+        <div class="flex"></div>
+        <SrCarContact></SrCarContact>
+    </div>
+    <!-- CAR DETAIL PAGE   -->
+</template>
+
+<script setup>
+useHead({
+    title: "route name"
+})
+
+//override default layout
+definePageMeta({
+    layout: "sr-browse"
+})
+
+//gets the whole route and puts it into variable route
+const route = useRoute()
+
+let id = route.path.split("/")[3]
+
+const cars = useCars()
+
+//filters...
+const car = cars.find((obj) => {
+    return obj.id == id;
+})
+
+console.log(car.price)
+</script>
+
+
+
+
+<style lang="scss" scoped></style>
