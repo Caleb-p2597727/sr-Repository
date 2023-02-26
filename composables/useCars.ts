108ddbdf74c91b76e2ce259ae91cb01ebbc7e01e
calebatsu <calebatsu@hotmail.it>
2023-02-26 21:42:28 +0000

    new composable created

---
+++ composables/useCars.ts
@@ -0,0 +1,12 @@
+//@ takes you to the root directory
+import cars from "../data/cars.json"
+
+//gets cars from cars.json
+export const useCars = () => {
+    return cars;
+}
+
+// export const useFoo = () => {
+//     return useState('foo', () => 'bar')
+//   }
+  
