895aeb7c22f73799489fa63171e86e3dc3c219d1
calebatsu <calebatsu@hotmail.it>
2023-02-26 21:49:52 +0000

    cars component created

---
+++ components/sr-cars.vue
@@ -0,0 +1,17 @@
+<template>
+    
+    <sr-car-summary v-for="(item, index) in list" :item="item" :index="index" :key="item.id" />
+</template>
+
+<script setup>
+
+const props = defineProps({
+    list: {
+        type: Array,
+        required: true
+    }
+})
+
+</script>
+
+<style lang="scss" scoped></style>
