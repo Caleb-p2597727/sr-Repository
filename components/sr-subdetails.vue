427ac42d680585a035eacce784060eb7f24d1254
calebatsu <calebatsu@hotmail.it>
2023-02-27 11:44:20 +0000

    pampered landing page style

--- components/sr-subdetails.vue
+++ components/sr-subdetails.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="grid grid-col-12 m-2">
-        <div class=" grid grid-col-2 bg-gray-200  border-2 border-slate-300 border-opacity-60 overflow-hidden rounded-lg bg-white shadow-lg dark:bg-neutral-700 w-full justify-around">
-            <div class="flex">
-                <img class="lg:h-72 md:h-48 w-full object-cover object-center" src="sr-card-pic.jpeg" alt="" />
+    <div class="m-2 space-y-8 p-2">
+        <div class=" grid grid-cols-12 gap-6 bg-gray-200  border-2 border-slate-300 border-opacity-60 overflow-hidden rounded-lg bg-white shadow-lg dark:bg-neutral-700  ">
+            <div class="col-span-7">
+                <img class=" w-full object-cover object-center" src="sr-card-pic.jpeg" alt="" />
             </div>
-            <div class="flex">
-                <div class="p-1 hover:bg-sky-100 hover:text-white transition duration-300 ease-in">
-                    <h5 class="mb-2 text-xl text-center font-medium text-neutral-800 dark:text-neutral-50">
+            <div class=" col-span-5">
+                <div class="m-2 p-2 hover:bg-sky-100 hover:text-white transition duration-300 ease-in">
+                    <h5 class="mb-2 mt-4 text-2xl text-center font-medium text-neutral-800 dark:text-neutral-50">
                         Find the Perfect Car for Your Needs
                     </h5>
-                    <p class="mb-4 text-base text-center text-neutral-600 dark:text-neutral-200">
+                    <p class="mb-4 text-base text-center text-xl text-neutral-600 dark:text-neutral-200">
                         At SpeedyRentals, we offer a wide range of vehicles to choose from, so you can find the perfect car
                         for your specific needs.
                         Whether you need a compact car for city driving, a spacious SUV for a family vacation, or a
@@ -20,15 +20,13 @@
                 </div>
             </div>
         </div>
-        <div
-            class=" bg-gray-200 border-2 border-slate-300 border-opacity-60 overflow-hidden rounded-lg bg-white shadow-lg dark:bg-neutral-700 w-full flex justify-around items-center">
-            <div class="   w-80 p-2 flex flex-col justify-center items-center">
-                <img class="lg:h-72 md:h-48 w-full object-cover object-center" src="sr-handkey.jpeg" alt="" />
-                <div class="p-1 hover:bg-sky-100 hover:text-white transition duration-300 ease-in">
-                    <h5 class="mb-2 text-xl text-center font-medium text-neutral-800 dark:text-neutral-50">
+        <div class=" grid grid-cols-12 gap-4 bg-gray-200  border-2 border-slate-300 border-opacity-60 overflow-hidden rounded-lg bg-white shadow-lg dark:bg-neutral-700  ">
+            <div class=" col-span-5">
+                <div class="m-2 p-2 hover:bg-sky-100 hover:text-white transition duration-300 ease-in">
+                    <h5 class="mb-2 mt-4 text-2xl text-center font-medium text-neutral-800 dark:text-neutral-50">
                         Earn Money by Renting Out Your Car
                     </h5>
-                    <p class="mb-4 text-base text-center text-neutral-600 dark:text-neutral-200">
+                    <p class="mb-4 text-base text-xl text-center text-neutral-600 dark:text-neutral-200">
                         With SpeedyRentals, you can turn your car into a source of income by renting it out to other users.
                         We offer a safe and secure platform for car owners to list their vehicles and connect with potential
                         renters.
@@ -39,16 +37,20 @@
                     </p>
                 </div>
             </div>
+            <div class="col-span-7">
+                <img class=" w-full object-cover object-center" src="sr-handkey.jpeg" alt="" />
+            </div>
         </div>
-        <div
-            class=" bg-gray-200 border-2 border-slate-300 border-opacity-60 overflow-hidden rounded-lg bg-white shadow-lg dark:bg-neutral-700 w-full flex justify-around items-center">
-            <div class="   w-80 p-2 flex flex-col justify-center items-center">
-                <img class="lg:h-72 md:h-48 w-full object-cover object-center" src="sr-fast-service.jpeg" alt="" />
-                <div class="p-1 hover:bg-sky-100 hover:text-white transition duration-300 ease-in">
-                    <h5 class="mb-2 text-xl text-center font-medium text-neutral-800 dark:text-neutral-50">
+        <div class=" grid grid-cols-12 gap-4 bg-gray-200  border-2 border-slate-300 border-opacity-60 overflow-hidden rounded-lg bg-white shadow-lg dark:bg-neutral-700  ">
+            <div class="col-span-7">
+                <img class=" w-full object-cover object-center" src="sr-fast-service.jpeg" alt="" />
+            </div>
+            <div class=" col-span-5">
+                <div class="m-2 p-2 hover:bg-sky-100 hover:text-white transition duration-300 ease-in">
+                    <h5 class="m-2 mt-4 text-2xl text-center font-medium text-neutral-800 dark:text-neutral-50">
                         Experience a Seamless Rental Process
                     </h5>
-                    <p class="mb-4 text-base text-center text-neutral-600 dark:text-neutral-200">
+                    <p class="mb-4 text-base text-xl text-center text-neutral-600 dark:text-neutral-200">
                         At SpeedyRentals, we believe that renting a car should be a seamless and stress-free experience.
                         That's why we offer a user-friendly platform with intuitive navigation and helpful support
                         resources.
@@ -59,8 +61,6 @@
                 </div>
             </div>
         </div>
-
-
     </div>
 </template>
 
