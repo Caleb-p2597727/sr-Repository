073e892225585b906dd8dc37f6e455458b4505f8
calebatsu <calebatsu@hotmail.it>
2023-03-05 15:44:18 +0000

    updated browse layout

--- layouts/sr-browse.vue
+++ layouts/sr-browse.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <SrHeader />
+    <sr-defaultheader></sr-defaultheader>   
     <div class="mx-auto mt-4 max-w-9xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-4/5">
       <slot />
     </div>
