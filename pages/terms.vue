8b7bdc6359da9ec6e2559019474f72496f809ee2
calebatsu <calebatsu@hotmail.it>
2023-03-12 16:15:48 +0000

    added terms of service page

--- pages/terms.vue
+++ pages/terms.vue
@@ -1,8 +1,42 @@
 <template>
-    <div>
-        im terms
 
-    </div>
+  <div class="bg-gradient-to-b from-indigo-700 to-indigo-900 text-white">
+    <div class="max-w-4xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
+      <h1 class="text-4xl font-extrabold mb-8">Terms of Use</h1>
+
+      <section>
+        <h2 class="text-2xl font-bold mb-4">Use of Our Services</h2>
+        <p class="mb-6">You may use our services only for lawful purposes and in accordance with these terms. You agree not to use our services:</p>
+        <ul class="list-disc ml-8 mb-6">
+          <li class="mb-4">In any way that violates any applicable federal, state, local, or international law or regulation.</li>
+          <li class="mb-4">To engage in any fraudulent, abusive, or otherwise illegal activity.</li>
+          <li class="mb-4">To post or transmit any content that is infringing, defamatory, or otherwise objectionable.</li>
+          <li>To interfere with or disrupt our services or servers, or to attempt to circumvent security measures.</li>
+        </ul>
+      </section>
+
+      <section>
+        <h2 class="text-2xl font-bold mb-4">User Accounts</h2>
+        <p class="mb-6">To use some of our services, you may need to create a user account. You are responsible for maintaining the confidentiality of your account information and for all activities that occur under your account. You agree to notify us immediately of any unauthorized use of your account or any other breach of security.</p>
+      </section>
+
+      <section>
+        <h2 class="text-2xl font-bold mb-4">Intellectual Property</h2>
+        <p class="mb-6">All content and materials on our website and services, including text, graphics, logos, images, and software, are the property of SpeedyRentals or its licensors and are protected by copyright and other intellectual property laws.</p>
+      </section>
+
+      <section>
+        <h2 class="text-2xl font-bold mb-4">Limitation of Liability</h2>
+        <p class="mb-6">SpeedyRentals will not be liable for any damages of any kind arising from the use of our website or services, including but not limited to direct, indirect, INCIDENTAL, PUNITIVE, and consequential damages. We make no warranties or representations about the accuracy or completeness of the content on our website or services.</p>
+      </section>
+
+      <section>
+        <h2 class="text-2xl font-bold mb-4">Indemnification</h2>
+        <p class="mb-6">You agree to indemnify and hold harmless SpeedyRentals and its officers, directors, employees, and agents from any claims, actions, suits, or demands, including reasonable attorneys' fees, made by any third </p>
+        </section>
+</div>
+
+</div>
 </template>
 
 <script>
