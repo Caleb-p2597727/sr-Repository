e1837fb9f1fc15d8e700652aebf70261c52c2a32
calebatsu <calebatsu@hotmail.it>
2023-03-25 20:52:07 +0000

    improved sidebar

--- components/sr-sidebar.vue
+++ components/sr-sidebar.vue
@@ -1,20 +1,21 @@
 <template>
   <div
-    class="grid grid-cols-1 border border-sky-700 divide-sky-700 rounded-lg divide-y p-1 gap-x-4 relative w-2/5 mr-10 h-3/5"
+    class="grid grid-cols-1 border border-sky-700 divide-y divide-sky-700 rounded-lg p-1 gap-x-4 relative w-2/5 mr-10 h-3/5"
   >
+  <!-- LOCATION STARTS -->
     <div>
-      <h3 class="divide-y-2 px-4 py-2">City</h3>
+      <h3 class="px-4 py-2">City</h3>
       <div
-        class="relative m-1 mb-4 p-2 py-2 border-gray-200 flex w-full items-stretch"
+        class="relative m-1 mb-4 p-2 flex w-full"
       >
         <span
-          class="flex items-center whitespace-nowrap rounded-l border border-r-0 border-solid border-neutral-300 px-3 py-[0.25rem] text-center text-base font-normal leading-[1.6] text-neutral-700 dark:border-neutral-600 dark:bg-zinc-800 dark:text-neutral-200 dark:placeholder:text-neutral-200"
+          class="flex items-center whitespace-nowrap rounded-l border border-r-0 border-solid border-neutral-300 px-3 py-[0.25rem] text-center text-base font-normal leading-[1.6] text-neutral-700 "
           id="basic-addon1"
           >{{ route.params.city }}</span
         >
         <input
           type="text"
-          class="relative m-0 w-0.5 min-w-0 flex-auto rounded-r border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition ease-in-out focus:z-[3] focus:border-primary-600 focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200"
+          class="relative m-0 w-0.5 min-w-0 flex-auto rounded-r border border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition ease-in-out focus:z-[3] focus:border-primary-600 focus:text-neutral-700 focus:shadow-te-primary focus:outline-none"
           placeholder="City"
           aria-label="City"
           aria-describedby="basic-addon1"
@@ -23,12 +24,13 @@
         />
       </div>
     </div>
-
+    <!-- LOCATION ENDS -->
+    <!-- MAKE ENDS -->
     <div class="px-4 py-2 mb-6">
-      <h3 class="font-bold mb-4">Make</h3>
-      <div class="flex">
+      <h3 class="font-bold mb-4">Make </h3>
+      <div class="grid">
         <select
-          class="border border-gray-300 flex-grow p-2 rounded text-gray-600 text-sm"
+          class="border border-neutral-300 flex-grow p-2 rounded text-gray-600 text-sm"
           data-te-select-init
           multiple
         >
@@ -42,8 +44,10 @@
         </select>
       </div>
     </div>
+    <!-- MAKE ENDS -->
 
-    <div class="px-4 py-2 mb-6">
+    <!-- PRICE STARTS -->
+    <div class="px-4 py-2 mb-6 ">
       <h3 class="font-bold mb-4">Price</h3>
       <div class="grid grid-cols-3 gap-4">
         <div class="flex items-center justify-center pl-3">
@@ -51,7 +55,7 @@
           <input
             type="number"
             min="0"
-            class="border border-gray-300 rounded w-20 mx-2 px-2 py-1 text-sm"
+            class="border border-neutral-300 rounded  w-20 mx-2 px-2 py-1 text-sm"
             placeholder="Min"
           />
         </div>
@@ -61,18 +65,19 @@
           <input
             type="number"
             min="0"
-            class="border border-gray-300 rounded w-20 mx-2 px-2 py-1 text-sm"
+            class="border border-neutral-300 rounded w-20 mx-2 px-2 py-1 text-sm"
             placeholder="Max"
           />
         </div>
       </div>
     </div>
+    <!-- PRICE ENDS -->
 
     <div class="grid place-items-center px-4 py-4 mb-2">
       <button
         @click="onChangeLocation"
         type="submit"
-        class="px-4 py-2 bg-indigo-500 text-white font-semibold rounded-md hover:bg-indigo-600"
+        class="px-4 py-2 bg-indigo-300 text-white font-semibold rounded-md hover:bg-indigo-600"
       >
         Show all results
       </button>
