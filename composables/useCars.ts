68d946412a8197bc6ca00d73ef1d4e8cd2b92b5a
calebatsu <calebatsu@hotmail.it>
2023-04-07 19:29:06 +0100

    listing cars are now displayed

--- composables/useCars.ts
+++ composables/useCars.ts
@@ -1,9 +1,14 @@
 //@ takes you to the root directory
 import cars from "../data/cars.json"
+import listings from "../data/listings.json"
 
 //gets cars from cars.json
 export const useCars = () => {
-    return cars;
+    return {
+        cars,
+        listings
+    };
+    
 }
 
 export const useCarMakes = () => {
--- pages/profile/listings/index.vue
+++ pages/profile/listings/index.vue
@@ -4,13 +4,13 @@
       <h1 class="text-6xl">My Listings</h1>
       <NuxtLink
         to="/profile/listings/create"
-        class="w-9 h-9 bg-blue-400 flex justify-center items-center rounded-full text-white font-bold cursor-pointer"
+        class="w-9 h-9 bg-blue-400 flex justify-center items-center rounded-full font-bold cursor-pointer"
         >+</NuxtLink
       >
     </div>
     <div class="shadow rounded p-3 mt-5">
-        
-      <CarListingCard
+        <!-- all cars within listings are displayed -->
+      <ListingCard
         v-for="listing in listings"
         :key="listing.id"
         :listing="listing"
@@ -23,6 +23,6 @@
 definePageMeta({
   layout: "custom",
 });
-
+//uses listings data
 const { listings } = useCars();
 </script>
