600dc9f5b98b6ccd89e8bbbb472d4abf1cc4cae7
calebatsu <calebatsu@hotmail.it>
2023-04-10 13:42:55 +0100

    added supabase module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,4 +6,9 @@ export default defineNuxtConfig({
         '@nuxt/image-edge',
         "@vueuse/nuxt", 
     ]
+    
+    // supabase: {
+    //     url: 'https://ntfctrcbyqfkinnqjqql.supabase.co',
+    //     key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50ZmN0cmNieXFma2lubnFqcXFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA5MDA0MjAsImV4cCI6MTk5NjQ3NjQyMH0.XaooMXCDvTzpej2QxKxBJq5v6eOfqfURcV_cTvIpMMU',
+    //   }
 })
