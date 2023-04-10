f140030ee35fca3239eb65945b1edf9ef4d81834
calebatsu <calebatsu@hotmail.it>
2023-04-10 13:43:21 +0100

    created function for log in and register buttons

--- components/sr-header.vue
+++ components/sr-header.vue
@@ -20,8 +20,12 @@
           <NuxtLink to="/" class="text-base font-medium text-gray-500 hover:text-gray-900">Browse</NuxtLink>
           <NuxtLink to="/" class="text-base font-medium text-gray-500 hover:text-gray-900">Home</NuxtLink>
         </nav>
-        <div class="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-          <NuxtLink to="/login" class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">Login</NuxtLink>
+        <div v-if="user" class="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+          <NuxtLink to="/profile/listings" class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">Profile</NuxtLink>
+          <p @click="logout" class="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-slate-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-slate-700">Log out</p>
+        </div>
+        <div v-else class="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+          <p @click="login" class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">Login</p>
           <NuxtLink to="/register" class="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-slate-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-slate-700">Register</NuxtLink>
         </div>
       </div>
@@ -41,6 +45,40 @@ const props = defineProps({
         default: 'bg-white'
     }
 })
+
+
+
+const user = useSupabaseUser()
+const supabase = useSupabaseClient()
+
+// const supabase = useSupabaseAuthClient()
+// const user = useSupabaseUser()
+
+const login = async () => {
+    console.log("clicked");
+    
+    const {error} = supabase.auth.signInWithOAuth({
+        provider: "google"
+    })
+
+    if (error) {
+        console.log(error);
+    }
+}
+
+const logout = () => {
+  //make user.value = null
+  //remove JWT from cookie browser
+  const {error} = supabase.auth.signOut()
+
+  if(error) {
+    console.log(error);
+  }
+  //navigate to home page
+  navigateTo('/')
+}
+
+
 
 </script>
 
