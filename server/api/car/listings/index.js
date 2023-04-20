5dc32cd89aabcedc90bea484997fe1beea189521
calebatsu <calebatsu@hotmail.it>
2023-04-20 01:46:19 +0100

    using input to create car in database

---
+++ server/api/car/listings/index.js
@@ -0,0 +1,70 @@
+import Joi from "joi";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+//we pass in everything we want to validate
+const schema = Joi.object({
+    make: Joi.string().required(),
+    model: Joi.string().required(),
+    year: Joi.number()
+        .min(1886)
+        .max(new Date().getFullYear() +1),
+    miles: Joi.number().required().min(0),
+    city: Joi.string().min(2).required(),
+    numberOfSeats: Joi.number().max(1000).min(1).required(),
+    description: Joi.string().min(20).required(),
+    features: Joi.array().items(Joi.string()).required,
+    image: Joi.string().required(),
+    listerId: Joi.string().required(),
+    price: Joi.number().min(0).required(),
+    name: Joi.string().required(),
+})
+
+export default defineEventHandler(async(event) => {
+    //gives us all data within the body
+    const body = await readBody(event);
+
+    const {error, value} = await schema.validate(body)
+
+    //if we get an error in input
+    if(error){
+        throw createError({
+            statusCode:412,
+            statusMessage: error.message,
+        })
+    }
+
+    const{     
+        image   ,        
+        name , 
+        numberOfSeats  ,
+        miles   ,        
+        price   ,        
+        features  ,     
+        description  ,   
+        city  ,          
+        make ,           
+        model     ,      
+        listerId         
+    } = body
+
+    const car = await prisma.car.create({
+        data:{
+            image   ,        
+            name , 
+            numberOfSeats  ,
+            miles   ,        
+            price   ,        
+            features  ,     
+            description  ,   
+            city: city.toLowerCase()  ,          
+            make ,           
+            model     ,      
+            listerId  
+        }
+
+    })
+
+    return {value,error};
+})
--- server/api/car/listings/index.ts
+++
@@ -1,7 +0,0 @@
-
-export default defineEventHandler(async(event) => {
-    //gives us all data within the body
-    const body = await readBody(event);
-
-    return body;
-})
