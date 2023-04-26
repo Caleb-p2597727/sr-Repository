7ad9889d1cc0df61528cc1aa78e5f03b23c71bc0
calebatsu <calebatsu@hotmail.it>
2023-04-26 22:07:50 +0100

    added comments to code

--- components/Car/Ad/Input.vue
+++ components/Car/Ad/Input.vue
@@ -3,6 +3,8 @@
 <template>
   <div class="flex flex-col w-[48%] mt-2">
     <label for="" class="text-cyan-500 mb-1 text-sm">{{ title }}</label>
+    <!-- Bind the value of the 'state' reactive variable to the input field so that it updates automatically as the user types -->
+    <!-- Use the value of the 'name' prop as the name attribute for the input field -->
     <input
       type="text"
       class="p-2 border w-100 rounded"
@@ -21,7 +23,9 @@ const props = defineProps({
   placeholder: String,
 });
 
-const emits = defineEmits(["changeInput"]);
+//  Define an 'emits' object with one property: 'changeInput', which is an event emitted when the input field changes
+const emits = defineEmits(["changeInput"]); 
+// Define a reactive variable called 'state' with an initial value of an empty string
 const state = ref("");
 const onChange = () => {
   emits("changeInput", state.value, props.name);
