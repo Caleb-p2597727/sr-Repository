2319c196297584b92d8e0bf7f850071875d268b4
calebatsu <calebatsu@hotmail.it>
2023-04-26 20:56:56 +0100

    review left message is rendered once review is left

--- pages/reviews.vue
+++ pages/reviews.vue
@@ -1,43 +1,48 @@
 <template>
-    <section>
-      <div class="h-screen text-gray-800">
-        <div
-          class="flex xl:justify-center lg:justify-between flex-wrap h-auto justify-center"
-        >
-        <h1>Leave your review</h1>
-          <!-- main div -->
-          <div class="h-52 w-92 px-3 rounded-2xl mt-12 justify-center">
-            <sr-star :rating="0" :star-size="'3rem'" class="ml-5"></sr-star>
-            <br />
-  
-            <input type="input" ref="input" class="h-32 w-72 border-2" />
-            <br />
-            <button
-              type="button"
-              class="px-7 py-3 mt-12 ml-14 bg-blue text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-              @click="handleReview"
-            >
-              Leave review
-            </button>
-          </div>
-        </div>
-      </div>
-    </section>
-  </template>
-  
-  <script>
+  <div class="h-screen flex-col text-gray-800">
+    <div>
+    <h2 class="text-2xl font-bold mb-10 mx-12 py-4 text-center">
+        Type your review
+      </h2>
+    </div>
+    <!-- main div -->
+    <div class="h-52 w-92 px-3 rounded-2xl mx-56">
 
-  
-  definePageMeta({
-    layout: "sr-browse",
-  });
+      <input type="input" ref="input" class="h-32 w-72 border-2" />
+      <br />
+      <button
+        type="button"
+        class="px-7 py-3 mt-12 ml-14 bg-blue text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+        @click="handleReview"
+      >
+        Leave review
+      </button>
+      <div v-if="reviewSubmitted" class="text-center mt-4 text-green-600 font-bold">
+    Review submitted!
+  </div>
+    </div>
+</div>
+</template>
 
-  const handleReview =  () => {
-    navigateTo('/')
-  }
-  
+<script setup>
 
-  </script>
-  
+import { ref } from 'vue';
+// import { navigateTo } from 'nuxt-link';
 
-  
+definePageMeta({
+  layout: "sr-browse",
+});
+
+useHead({
+  title: "Leave a review",
+});
+
+const reviewSubmitted = ref(false);
+
+const handleReview = () => {
+  reviewSubmitted.value = true;
+//   navigateTo("/");
+};
+</script>
+
+
