2ff9981f22cca761f4976b39b63a69869fe00c78
calebatsu <calebatsu@hotmail.it>
2023-04-26 14:30:13 +0100

    added reviews data and composable

--- composables/useCars.ts
+++ composables/useCars.ts
@@ -2,13 +2,15 @@
 import cars from "../data/cars.json"
 import listings from "../data/listings.json"
 import makes from "../data/makes.json"
+import reviews from "../data/reviews.json"
 
 //gets cars from cars.json
 export const useCars = () => {
     return {
         cars,
         listings,
-        makes
+        makes,
+        reviews
     };
 }
 
