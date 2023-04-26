44e8c87bd82a1e92de9b6f8f446331f3774227a8
calebatsu <calebatsu@hotmail.it>
2023-04-26 21:47:35 +0100

    updated text on page

--- pages/logger.vue
+++ pages/logger.vue
@@ -2,7 +2,7 @@
     <div class="mt-10">
         {{ user }}
         <h1 class="text-5xl font-bold mb-7">
-            Log in
+            Log in to continue
         </h1>
         <button @click="login" class="button bg-red-400 p-3 rounded text-white font-bold">
             Log in with Google
@@ -14,6 +14,10 @@
 definePageMeta({
     layout: "sr-browse"
 })
+
+useHead({
+  title: "Access Denied",
+});
 
 const supabase = useSupabaseAuthClient()
 const user = useSupabaseUser()
