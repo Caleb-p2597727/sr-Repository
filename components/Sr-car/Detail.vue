6d2329d2b358e4be70b91bb6d4a6cb263f026117
calebatsu <calebatsu@hotmail.it>
2023-04-26 20:11:16 +0100

    added link under listed car

--- components/Sr-car/Detail.vue
+++ components/Sr-car/Detail.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="mt-10">
         <Img :src="`https://fhokzwjlcbdialdbmjpq.supabase.co/storage/v1/object/public/images/${props.image}`" class="w-full" alt="" />
-        <h1 class="mt-10 text-4xl">{{ props.model }}</h1>
+        <h1 class="mt-10 text-4xl">{{ props.model }} </h1>
+        <NuxtLink class="py-8 text-xl underline" to="/hostProfile">
+          Host: Caleb
+
+          </NuxtLink>
         <div class="text-slate-500 flex text-lg mt-3 border-b pb-5 justify-between">
             <div class="flex">
                 <p class="mr-2">{{props.seats}} seat/s</p>
