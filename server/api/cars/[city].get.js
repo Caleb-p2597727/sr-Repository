53b79a242869f1a884d5f2c8fcb65725620d2e0f
calebatsu <calebatsu@hotmail.it>
2023-04-26 20:10:57 +0100

    added comments to endpoint

--- server/api/cars/[city].get.js
+++ server/api/cars/[city].get.js
@@ -1,19 +1,28 @@
+// Import the PrismaClient from the @prisma/client package
 import { PrismaClient } from '@prisma/client';
 
+// Create a new instance of the PrismaClient
 const prisma = new PrismaClient();
 
+// Define an event handler function that takes an event as input and returns a Promise
 export default defineEventHandler(async (event) => {
+    // Extract the 'city' parameter from the context of the event
     const { city } = event.context.params;
+    // Extract the 'make', 'minPrice', and 'maxPrice' query parameters from the event
     const { make, minPrice, maxPrice } = getQuery(event);
 
+    // Use Prisma to query the database for cars that match the specified criteria
     let filteredCars = await prisma.car.findMany({
         where: {
+            // Filter by city, ensuring that it matches the specified value (case-insensitive)
             city: {
                 equals: city.toLowerCase(),
             },
+            // Filter by make, if specified
             make: make ? {
                 equals: make,
             } : undefined,
+            // Filter by price, if minPrice and/or maxPrice are specified
             price: minPrice && maxPrice ? {
                 gte: parseInt(minPrice),
                 lte: parseInt(maxPrice),
@@ -24,6 +33,6 @@ export default defineEventHandler(async (event) => {
             } : undefined,
         },
     });
-
+    // Return the filtered cars
     return filteredCars;
 });
