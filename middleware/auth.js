bad0a99a0b7ef104cdbcc4c58d72012303eb2775
calebatsu <calebatsu@hotmail.it>
2023-04-26 20:10:41 +0100

    added comments

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,4 @@
+// if not loogged in you cant acces and you are redirected to /logger
 export default defineNuxtRouteMiddleware((to, from) => {
     const user = useSupabaseUser()
     if(user.value) {
