003edfc08fb078ec094b91d19ca39c7c986ea014
calebatsu <calebatsu@hotmail.it>
2023-04-26 12:41:39 +0100

    updated footer number and added comments

--- components/sr-footer.vue
+++ components/sr-footer.vue
@@ -1,15 +1,21 @@
 <template>
+  <!-- This is the footer component, with some styling classes applied to it -->
     <footer class="text-center lg:text-left bg-gray-100 text-gray-600 bg-neutral-200">
+      <!-- This is a flex container with some padding -->
         <div class="flex justify-center items-center  p-6 ">
+          <!-- This is a link to Facebook, with a Facebook icon -->
             <a href="#!" class="mr-6 text-gray-600">
+              <!-- This is an SVG icon -->
               <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="facebook-f"
                 class="w-2.5" role="img" xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 320 512">
+                <!-- This is a path that defines the shape and color of the icon -->
                 <path fill="currentColor"
                   d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z">
                 </path>
               </svg>
             </a>
+            <!-- This is a link to Twitter, with a Twitter icon -->
             <a href="#!" class="mr-6 text-gray-600">
               <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="twitter"
                 class="w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
@@ -18,6 +24,7 @@
                 </path>
               </svg>
             </a>
+            <!-- This is a link to Instagram, with a Instagram icon -->
             <a href="#!" class="mr-6 text-gray-600">
               <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="instagram"
                 class="w-3.5" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
@@ -26,15 +33,6 @@
                 </path>
               </svg>
             </a>
-            <a href="#!" class="text-gray-600">
-              <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github"
-                class="w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
-                <path fill="currentColor"
-                  d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z">
-                </path>
-              </svg>
-            </a>
-          
         </div>
         <div class="mx-6 py-10 text-center md:text-left">
           <div class="grid grid-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
@@ -98,7 +96,7 @@
                     d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z">
                   </path>
                 </svg>
-                + 01 234 567 88
+                0116 888333
               </p>
             </div>
           </div>
