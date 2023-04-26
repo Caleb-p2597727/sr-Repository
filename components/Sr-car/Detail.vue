2d30d4e2d882f8becc76d28e6e05e1e097d98b15
calebatsu <calebatsu@hotmail.it>
2023-04-26 10:56:16 +0100

    replaced with image retrieved from bucket

--- components/Sr-car/Detail.vue
+++ components/Sr-car/Detail.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="mt-10">
-        <Img :src="props.image" class="w-full" alt="" />
+        <Img :src="`https://fhokzwjlcbdialdbmjpq.supabase.co/storage/v1/object/public/images/${props.image}`" class="w-full" alt="" />
         <h1 class="mt-10 text-4xl">{{ props.model }}</h1>
         <div class="text-slate-500 flex text-lg mt-3 border-b pb-5 justify-between">
             <div class="flex">
